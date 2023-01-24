8148e7fc52cfe9253c0862a755fbdfe6e0e7543e
Shruti <shruti@simformsolutions.com>
2023-01-24 17:59:05 +0530

    Modified

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Pattern Code");
         for(int i = 0; i < 5; i++){
             for(int j = 0; j <= i; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.print("\n");
         }
